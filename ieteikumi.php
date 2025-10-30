3b3d41a2caa1a753d254ea0b8137de27bd4a06d4
Kaspars <ksokolovs50@gmail.com>
2025-10-31 00:00:56 +0200

    home page design update

--- ieteikumi.php
+++ ieteikumi.php
@@ -13,21 +13,24 @@ session_start();
     <link href="ieteikumi.css" rel="stylesheet">
 </head>
 <body>
-    <Header class="header">
-        <div class="home">
-            <button class="home_button"></button>
-        </div>
-        <div>
-            IETEIKUMI.lv
+    <div class="min-h-screen max-w-full">
+    <header class="header">
+        <div class="title">
+            Ieteikumi.lv
         </div>
         <div>
             <?php if (!isset($_SESSION['user_id'])): ?>
-    <a href="register.php"><button>Reģistrēties</button></a>
-    <a href="login.php"><button>Pieteikties</button></a>
+    <a href="register.php"><button class="boton-elegante">Reģistrēties</button></a>
+    <a href="login.php"><button class="boton-elegante">Pieteikties</button></a>
 <?php else: ?>
-    <a href="profile.php"><button>Profils</button></a>
-    <a href="logout.php"><button>Atteikties</button></a>
+    <a href="profile.php"><button class="boton-elegante">Profils</button></a>
+    <a href="logout.php"><button class="boton-elegante">Atteikties</button></a>
 <?php endif; ?>
         </div>
+    </header>
+    <div class="sidebar">
+       <a><div class="sidebar-options"><img width="25" height="25" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>Sākums</div></a>
+       <div class="sidebar-options"><img width="25" height="25" src="https://img.icons8.com/ios/50/cutlery.png" alt="cutlery"/>Ēdieni</div>
+    </div>
 </body>
 </html>
