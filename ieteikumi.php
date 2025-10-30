12ae1d56611d21266b69e00ab265e102f3aae30c
Kaspars <ksokolovs50@gmail.com>
2025-10-30 17:17:14 +0200

    first commit

---
+++ ieteikumi.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="ieteikumi.css" rel="stylesheet">
+</head>
+<body>
+    <Header class="header">
+        <div class="home">
+            <button class="home_button"></button>
+        </div>
+        <div>
+            IETEIKUMI.lv
+        </div>
+        <div>
+            
+        </div>
+</body>
+</html>
