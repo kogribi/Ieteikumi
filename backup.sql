c783cb7abf7a19f0f931a4415928091d6f0fa349
Kaspars <ksokolovs50@gmail.com>
2025-11-30 20:47:38 +0200

    databaze

--- backup.sql
+++ backup.sql
@@ -28,7 +28,7 @@ CREATE TABLE `likes` (
   `post_id` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_like` (`user_id`,`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `likes` (
 
 LOCK TABLES `likes` WRITE;
 /*!40000 ALTER TABLE `likes` DISABLE KEYS */;
-INSERT INTO `likes` VALUES (14,1,17),(18,1,21),(17,1,25),(15,3,17);
+INSERT INTO `likes` VALUES (1,1,1);
 /*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,13 +56,13 @@ CREATE TABLE `recommendations` (
   `genre` varchar(50) DEFAULT NULL,
   `time` varchar(50) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
-  `length` int DEFAULT NULL,
+  `length` varchar(50) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `description` text NOT NULL,
   `title` varchar(100) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +71,7 @@ CREATE TABLE `recommendations` (
 
 LOCK TABLES `recommendations` WRITE;
 /*!40000 ALTER TABLE `recommendations` DISABLE KEYS */;
-INSERT INTO `recommendations` VALUES (17,'Kaspars',1,1,'Ēdiens','1',4.00,0,'2025-11-25 16:39:23','sis ir lielesiokl','test','uploads/6925dbbb73c04.jpg'),(18,'Peteris1998',3,8,'Ēdiens','0',5.00,30,'2025-11-25 17:09:38','Klasiska itāļu pica ar sieru un tomātiem','Pica Margherita','uploads/6925e2d28b3bb.jpg'),(19,'Peteris1998',3,9,'Spēles','2',15.00,0,'2025-11-25 17:14:42','Minecraft ir “sandbox” spēle, ko izstrādājusi un publicējusi “Mojang Studios”. Formāli izlaista personālajiem datoriem 2011. gada 18. novembrī pēc sākotnējās publiskās alfa versijas izlaišanas 2009. gada 17. maijā, tā ir pārnesta uz daudzām platformām, tostarp mobilajām ierīcēm un dažādām videospēļu konsolēm.','Minecraft','uploads/6925e402aed00.webp'),(20,'Peteris1998',3,7,'Vietas','0',0.00,5,'2025-11-25 17:16:04','Lieliska vieta, kur iegādāties svaigus produktus','Rīgas Centrāltirgus','uploads/6925e4540d22c.jpg'),(21,'Peteris1998',3,8,'Aktivitāte','2',5.00,0,'2025-11-25 17:17:35','Relaksējoša aktivitāte svaigā gaisā','Jogas nodarbība parkā','uploads/6925e4af1929c.jpg'),(22,'Peteris1998',3,7,'Video','3',0.00,0,'2025-11-25 17:21:24','Rouza, kuru piespiež precēties ar bagātu vīrieti, iemīlas Džekā, talantīgā māksliniekā, uz nenogremdējamā Titānika klāja. Diemžēl kuģis ietriecas aisbergā, apdraudot viņu dzīvības.','Filma \"Titaniks\"','uploads/6925e594dbce2.jpeg'),(23,'Peteris1998',3,9,'Ēdiens','1',10.00,0,'2025-11-25 17:24:09','1. Malto gaļu izdauza pret galdu, lai tā paliktu viendabīgāka. Tad izveido no tās divas vienādas bumbinjas un saspiež tās starp pārtikas plēvi, lai izveidoajs divas plānas un apaļas gaļas plāksnites, kuras sasaldē.\r\n2. Sīpolu sagriež ripiņās un pārspiež ar citrona sulu.\r\n3. Kad gaļa sasalusi liek to uz karstas un ietaukotas pannas un pārkaisa ar sāli un pipariem, apcep no vienas puses, tad apgriež otrādi un atkal apkaisa.\r\n4. Maizītes pārgriež uz pusēm un apgrauzdē.\r\n5. Kārto: maizīte, gaļa, sīpoli, siers, salātlapa, majonēze/mērce pēc izveles, maizīte.\r\nBauda siltu\r\n\r\nLabu apetiiti!','Siera burgers','uploads/6925e6395c89e.jpg'),(24,'Peteris1998',3,7,'Vietas','0',0.00,5,'2025-11-25 17:26:09','Perfekta vieta prieks peldešanās, bet parasti daudz cilvēku.','Ninieris','uploads/6925e6b1836c3.png'),(25,'Peteris1998',3,8,'Spēles','32',20.00,0,'2025-11-25 17:29:58','Grand Theft Auto V ir 2013. gada piedzīvojumu spēle, ko izstrādāja Rockstar North un publicēja Rockstar Games. Tā ir septītā galvenā spēle Grand Theft Auto sērijā pēc 2008. gada Grand Theft Auto IV un kopumā piecpadsmitā daļa.','GTA V','uploads/6925e79696a71.avif');
+INSERT INTO `recommendations` VALUES (1,'Kaspars',1,8,'Produkti','0',0.00,'0','2025-11-30 17:45:28','6 versija no laragon ir briva un ir perfekta prieks iesacejiem','laragon','uploads/692c82b872481.webp');
 /*!40000 ALTER TABLE `recommendations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-25 20:18:43
+-- Dump completed on 2025-11-30 20:39:09
